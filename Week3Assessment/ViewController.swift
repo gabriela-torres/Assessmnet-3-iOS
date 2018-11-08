ecfad455df469ece97affe8f4e1b1062150f7dc6
Gabriela Torres <gabrielatorres@Interapts-MacBook-Pro-6.local>
2018-11-08 10:27:08 -0600

    First commit.

---
+++ Week3Assessment/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Week3Assessment
+//
+//  Created by Ryan Brashear on 1/3/18.
+//  Copyright © 2018 Ryan Brashear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    @IBOutlet weak var TableView: UITableView!
+    
+    let person = [Person(name: "Miranda" , favoriteThing: "Marshmellow my cat.", favoriteColor: .blue), Person(name: "Sawyer", favoriteThing: "Stella my cat", favoriteColor: .green), Person(name: "Gabby", favoriteThing: "Laptop", favoriteColor: .red)]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
